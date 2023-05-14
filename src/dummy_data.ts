2ac7da73d31742bc8623b12b1b8efa17636d67a0
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2023-05-14 17:15:37 +0200

    adds dummy todos

---
+++ src/dummy_data.ts
@@ -0,0 +1,19 @@
+import { TodoItemType } from "./types";
+
+export const TODOS: TodoItemType[] = [
+    {
+        id: 1,
+        text: "Learn React",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Learn Redux",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Learn React Router",
+        completed: false
+    }
+];
