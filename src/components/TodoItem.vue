f2a0df8cafef668a4ae4ff2d592cc2692447c090
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2023-05-14 17:15:18 +0200

    adds logic of adding todos

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,24 @@
 <template>
     <li class="todo-item">
-        <input type="checkbox" class="todo-item__checkbox" />
-        <span class="todo-item__text">Zrobić zakupy</span>
+        <input @change="updateTodo" type="checkbox" class="todo-item__checkbox" :checked="isChecked" />
+        <span class="todo-item__text">{{ item.text }}</span>
     </li>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {ref} from 'vue';
+import { TodoItemType } from '../types';
+
+const {item} = defineProps<TodoItemProops>();
+const isChecked = ref<boolean>(item.completed);
+
+interface TodoItemProops {
+    item: TodoItemType;
+}
+
+const updateTodo = () => {
+    isChecked.value = !isChecked.value;
+    item.completed = isChecked.value;
+};
+
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,25 +1,60 @@
 <template>
     <Card>
-        <div class="todo-list">
+        <div class="todo-list" ref="todo-list-ref">
             <h2 class="todo-list__title">
                 <p>Lista todo</p>
-                <p>Wykonane: 0</p>
+                <p>Wykonane: {{ completedTodos }}</p>
             </h2>
             <ul class="todo-list__list">
-                <TodoItem v-for="n in 5" :key="n" />
+                <TodoItem v-for="item in todos" :key="item.id" :item="item" />
             </ul>
             <div class="todo-list__add-item">
-                <button class="todo-list__add-item-button">
+                <button @click="addTodo" class="todo-list__add-item-button">
                     <CustomIcon name="plus" size="10" color="#bbb" />
                 </button>
-                <input type="text" placeholder="Dodaj nowy element checklisty" class="todo-list__add-item-input" />
+                <input
+                    @change="handleNewTodo"
+                    @keyup.enter="addTodo"
+                    :value="newTodoText"
+                    type="text"
+                    placeholder="Dodaj nowy element checklisty"
+                    class="todo-list__add-item-input" />
             </div>
         </div>
     </Card>
 </template>
 
 <script setup lang="ts">
+import {ref, computed} from 'vue';
 import Card from './Card.vue';
 import CustomIcon from './CustomIcon.vue';
 import TodoItem from './TodoItem.vue';
+import {TODOS} from '../dummy_data';
+import {TodoItemType} from '../types';
+
+const todos = ref<TodoItemType[]>(TODOS);
+const newTodoText = ref<string>('');
+const todoListRef = ref<HTMLElement | null>(null);
+
+const handleNewTodo = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    newTodoText.value = target.value;
+};
+
+const addTodo = () => {
+    if (!newTodoText.value.trim()) {
+        newTodoText.value = '';
+        return;
+    }
+    const todo: TodoItemType = {
+        id: Math.random(),
+        text: newTodoText.value,
+        completed: false,
+    };
+    todos.value.push(todo);
+    newTodoText.value = '';
+    todoListRef.value?.scrollTo(0, todoListRef.value.scrollHeight);
+};
+
+const completedTodos = computed(() => todos.value.filter((todo) => todo.completed).length);
 </script>
