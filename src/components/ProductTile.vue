fea219a75820f6376bc03313c13428f933a7620e
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2023-05-14 12:09:07 +0200

    adds products components

---
+++ src/components/ProductTile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="product-tile">
+        <Card>
+            <div class="product-tile__title">iPhone 6s Plus 16GB</div>
+            <img class="product-tile__image" src="../assets/imgs/img1.png" alt="iPhone 6s Plus 16GB" />
+            <div class="product-tile__prices">
+                <div class="product-tile__regular-price">649 $</div>
+                <div class="product-tile__regular-price--discount">1000 $</div>
+            </div>
+            <div class="product-tile__discount-badge">-35%</div>
+        </Card>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue';
+</script>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="products-list">
+        <Card>
+            <h2 class="products-list__title">Lista Produktów</h2>
+            <table class="products-list__table">
+                <thead>
+                    <tr class="products-list__table-header">
+                        <th>#</th>
+                        <th>NAZWA</th>
+                        <th>PROMOCYJNA CENA</th>
+                        <th>CENA</th>
+                        <th>WALUTA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>0</td>
+                        <td>iPhone 6s Plus 16GB</td>
+                        <td>649</td>
+                        <td>1000</td>
+                        <td>$</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>iPad Pro 32GB</td>
+                        <td>600</td>
+                        <td>800</td>
+                        <td>$</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>MacBook Pro</td>
+                        <td></td>
+                        <td>8000</td>
+                        <td>PLN</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Card>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Card from './Card.vue';
+</script>
