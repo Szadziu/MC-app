682f3192990734516e7d776512b07b1b4533ba75
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2023-05-14 12:07:54 +0200

    adds default layout component

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="default-layout">
+        <h1 class="default-layout__title">{{ title }}</h1>
+        <div class="default-layout__container">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface DefaultLayoutProps {
+    title: string;
+}
+
+defineProps<DefaultLayoutProps>();
+</script>
