fc1f10d36b800a271ff52961b4568a7f3dc9acac
Maciej Mikołajczak <mcj.mikolajczak@gmail.com>
2023-05-14 12:07:29 +0200

    adds default styles

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <DefaultLayout title="Muscode App">
+            <TodoList />
+            <ProductsList />
+            <Products />
+        </DefaultLayout>
+        <ModalDialog />
+    </div>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from './layouts/DefaultLayout.vue';
+import Products from './components/Products.vue';
+import ProductsList from './components/ProductsList.vue';
+import TodoList from './components/TodoList.vue';
+import ModalDialog from './components/ModalDialog.vue';
+</script>
